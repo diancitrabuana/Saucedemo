8951ad3118830add67d1bc2a1f9f5abb1c02b0d1
diancitrabuana <diandcb4@gmail.com>
2023-10-22 22:48:47 +0700

    new print Namaku.java

--- src/main/java/firstProgram/Namaku.java
+++ src/main/java/firstProgram/Namaku.java
@@ -3,5 +3,6 @@ package firstProgram;
 public class Namaku {
     public static void main(String[] args){
         System.out.println("Namaku Dian Citra Buana");
+        System.out.println("Belajar git");
     }
 }
